c62c635a704e3bfe1c76d4e02e347af40687d4e4
Eddo Hintoso <eddo.hintoso@gmail.com>
2016-04-15 16:58:51 +0800

    Boilerplate with Foundation CSS

---
+++ src/js/components/App.jsx
@@ -0,0 +1,64 @@
+/**
+ * Main application
+ */
+
+
+var React = require('react');
+
+// Flux elements
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+// Components
+// var SubComponent = require('./SubComponent.jsx');
+
+
+function getAppState() {
+    return {
+
+    };
+}
+
+
+var App = React.createClass({
+
+    getInitialState: function() {
+        return getAppState();
+    },
+
+    componentDidMount: function() {
+        AppStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+        AppStore.removeChangeListener(this._onChange);
+    },
+
+    // Update view state when change is received
+    _onChange: function() {
+        this.setState(getAppState());
+    },
+
+    render: function() {
+        return (
+            <div>
+                <div className="off-canvas-wrapper">
+                    <div className="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+                        <div className="off-canvas position-left reveal-for-large" data-off-canvas data-position="left">
+                            <div className="row column">
+                                <br />
+                                // ADD NOTE FORM
+                            </div>
+                        </div>
+                        <div className="off-canvas-content" data-off-canvas-content>
+                            // NOTELIST
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+});
+
+module.exports = App;
